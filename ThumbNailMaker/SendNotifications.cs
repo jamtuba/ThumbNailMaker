2c6a2b9ec21ddfa61d919c6ad0be47074b880763
jamtuba <jamtuba@gmail.com>
2020-10-14 17:15:31 +0200

    Added attachment to email

--- ThumbNailMaker/SendNotifications.cs
+++ ThumbNailMaker/SendNotifications.cs
@@ -2,7 +2,9 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using SendGrid.Helpers.Mail;
+using System;
 using System.IO;
+using System.Text;
 
 namespace ThumbNailMaker
 {
@@ -39,6 +41,13 @@ namespace ThumbNailMaker
                 "Best Regards," + "<br>" + "Website Team";
 
             message.AddContent("text/html", emailContent);
+
+            message.AddAttachment(firstName + "_" + lastName + ".log", Convert.ToBase64String(Encoding.UTF8.GetBytes(emailContent)),
+                "text/plain",
+                "attachment",
+                "Logs"
+                );
+
             using (var emailLogBloboutput = binder.Bind<TextWriter>(new BlobAttribute($"userregistrationmaillogs/{ inputJson.RowKey }.log")))
             {
                 emailLogBloboutput.WriteLine(emailContent);
